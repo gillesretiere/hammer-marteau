6690c63280f0f7d5b84857b177fbc4d372140dc4
gillesretiere <gilles.retiere@gmail.com>
2025-05-29 13:40:01 +0000

    Drawer MUI removed Replaced by simple JS Drawer

--- src/components/Appbar.jsx
+++ src/components/Appbar.jsx
@@ -9,7 +9,6 @@ import Container from "@mui/material/Container";
 import Toolbar from "@mui/material/Toolbar";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from '@mui/icons-material/Menu';
-import Drawer from "@mui/material/Drawer";
 import Box from "@mui/material/Box";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
@@ -17,10 +16,12 @@ import ListItemText from "@mui/material/ListItemText";
 import DescriptionIcon from "@mui/icons-material/Description";
 import CloseIcon from "@mui/icons-material/Close";
 import Divider from "@mui/material/Divider";
+import Drawer from './Drawer';
+import Card from './Card';
 
-const AppNavBar = ({ toggleLightDarkMode }) => {
+const Appbar = ({ toggleLightDarkMode }) => {
     //react useState hook to save the current open/close state of the drawer, normally variables dissapear afte the function was executed
-    const [open, setState] = useState(false);
+    const [open, setOpen] = useState(false);
     //function that is being called every time the drawer should open or close, the keys tab and shift are excluded so the user can focus between the elements with the keys
     const toggleDrawer = (open) => (event) => {
         if (
@@ -30,11 +31,11 @@ const AppNavBar = ({ toggleLightDarkMode }) => {
             return;
         }
         //changes the function state according to the value of open
-        setState(open);
+        setOpen(open);
     };
     return (
         <>
-            <AppBar position="static" sx={{ bgcolor: '#fff' }} className="border-gray-200 dark:bg-gray-900">
+            <AppBar position="static" sx={{ bgcolor: '#fff' }} className="border-gray-200 dark:bg-zinc-900">
                 <Container maxWidth="xl" disableGutters="true">
                     <Toolbar>
                         {/* LOGO */}
@@ -62,6 +63,43 @@ const AppNavBar = ({ toggleLightDarkMode }) => {
                                 <MenuIcon />
                             </IconButton>
                         </div>
+                        {/* The outside of the drawer */}
+                        <div className="dark:bg-zinc-900">
+                            <Drawer className="dark:bg-zinc-900" open={open} setOpen={setOpen}>
+                                <Box
+                                    sx={{
+                                        p: 2,
+                                        height: 1,
+                                    }}
+                                >
+                                    {/* when clicking the icon it calls the function toggleDrawer and closes the drawer by setting the variable open to false */}
+                                    <IconButton sx={{ mb: 2 }} className='dark:text-white'>
+                                        <CloseIcon onClick={toggleDrawer(false)} />
+                                    </IconButton>
+
+                                    <Divider sx={{ mb: 2 }} />
+                                    <Box
+                                        sx={{ mb: 2 }}>
+                                        {NavbarData && NavbarData.map(
+                                            (item, index) => (
+                                                <>
+                                                    {/* on teste si href interne (#) : composant "a" sinon "Link" */}
+                                                    <ListItemButton key={index} component={item.href.startsWith('#') ? "a" : Link} href={`${item.href}`} to={`${item.href}`}
+                                                        className='dark:text-white dark:hover:bg-zinc-700' >
+                                                        <ListItemIcon>
+                                                            <DescriptionIcon sx={{ color: "primary.contrastText" }} />
+                                                        </ListItemIcon>
+                                                        <ListItemText primary={`${item.title}`} />
+                                                        {/* <Chip label={item.enabled ? "disponible" :"prochainement"} size="small"></Chip> */}
+                                                    </ListItemButton>
+                                                </>
+                                            ))}
+                                    </Box>
+
+                                </Box>
+                            </Drawer>
+                        </div>
+
                         {/* Menu Options */}
                         <div class="hidden w-full md:block md:w-auto mr-5" id="navbar-default">
                             <ul class="font-articulat_cf font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
@@ -80,50 +118,8 @@ const AppNavBar = ({ toggleLightDarkMode }) => {
                                 })}
                             </ul>
                         </div>
-                        {/* The outside of the drawer */}
-                        <Drawer
-                        className="dark:bg-gray-900"
-                            //from which side the drawer slides in
-                            anchor="right"
-                            //if open is true --> drawer is shown
-                            open={open}
-                            //function that is called when the drawer should close
-                            onClose={toggleDrawer(false)}
-                        //function that is called when the drawer should open
-                        // onOpen={toggleDrawer(true)}
-                        >
-                            {/* The inside of the drawer */}
-                            <Box
-                                className="dark:bg-gray-900"
-                                sx={{
-                                    p: 2,
-                                    height: 1,
-                                }}
-                            >
-                                {/* when clicking the icon it calls the function toggleDrawer and closes the drawer by setting the variable open to false */}
-                                <IconButton sx={{ mb: 2 }}>
-                                    <CloseIcon onClick={toggleDrawer(false)} />
-                                </IconButton>
 
-                                <Divider sx={{ mb: 2 }} />
-                                <Box sx={{ mb: 2 }}>
-                                    {NavbarData && NavbarData.map(
-                                        (item, index) => (
-                                            <>
-                                                {/* on teste si href interne (#) : composant "a" sinon "Link" */}
-                                                <ListItemButton key={index} component={item.href.startsWith('#') ? "a" : Link} href={`${item.href}`} to={`${item.href}`}  >
-                                                    <ListItemIcon>
-                                                        <DescriptionIcon sx={{ color: "primary.contrastText" }} />
-                                                    </ListItemIcon>
-                                                    <ListItemText primary={`${item.title}`} />
-                                                    {/* <Chip label={item.enabled ? "disponible" :"prochainement"} size="small"></Chip> */}
-                                                </ListItemButton>
-                                            </>
-                                        ))}
-                                </Box>
 
-                            </Box>
-                        </Drawer>
                         <LightDarkToggle toggleLightDarkMode={toggleLightDarkMode} />
 
                     </Toolbar>
@@ -133,4 +129,4 @@ const AppNavBar = ({ toggleLightDarkMode }) => {
     )
 }
 
-export default AppNavBar
+export default Appbar
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import Navbar from './Navbar';
-import AppNavBar from './AppNavBar';
+import Appbar from './Appbar';
 
 const Header = ({toggleLightDarkMode}) => {
     return (
         <>
-            <AppNavBar toggleLightDarkMode = {toggleLightDarkMode} />
+            <Appbar toggleLightDarkMode = {toggleLightDarkMode} />
         </>
     )
 }
